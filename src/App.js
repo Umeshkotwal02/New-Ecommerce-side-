39f042151e90b0c0dfcb60427a87cd0b1026b85c
umeshkotwal_07 <umeshkotwal7@gmail.com>
2024-09-26 00:34:59 +0530

    Add Initial Stage

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navapp from "./components/Navbar";
+import Product from "./components/Product"; // Correctly import the component
+import Cart from "./components/Cart";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Home from "./components/Home";
+import ProductDetails from "./components/ProductDetails";
+import UserList from "./components/UserList";
+import Login from "./components/Login";
+import SignUp from "./components/Sign";
+import Profile from "./components/Profile";
+// import ProductCategory from "./components/Product";
+
+function App() {
+  // Initialize state from localStorage
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("isLoggedIn") === "true"
+  );
+
+  // Update localStorage whenever the login status changes
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", isLoggedIn);
+  }, [isLoggedIn]);
+
+  return (
+    <Router>
+      <Navapp isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            isLoggedIn ? (
+              <Navigate to="/product"/>
+            ) : (
+              <Login setIsLoggedIn={setIsLoggedIn} />
+            )
+          }
+        />
+           <Route
+          path="/sign"
+          element={<SignUp />}
+        />
+        <Route path="/product" element={<Product />} />
+        <Route
+          path="/profile"
+          element={isLoggedIn ? <Profile /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/home"
+          element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route path="/sign" element={<SignUp />} />
+        <Route
+          path="/about"
+          element={isLoggedIn ? <About /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/users"
+          element={isLoggedIn ? <UserList /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/cart"
+          element={isLoggedIn ? <Cart /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+export default App;
